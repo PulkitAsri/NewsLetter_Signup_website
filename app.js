225c001999cf38af13cd90e337a2ace7f3d112aa
PulkitAsri <pulkit.asri@gmail.com>
2021-05-11 00:19:11 +0530

    Added success and failure pages

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const https=require("https");
+const https = require("https");
 
 
 
@@ -24,34 +24,34 @@ app.post("/", function (req, res) {
     const email = req.body.email;
 
     const data = {
-        members: [
-            {
+        members: [{
             email_address: email,
             status: "subscribed",
             merge_fields: {
                 FNAME: firstName,
                 LNAME: lastName
-                }   
             }
-        ]
+        }]
     };
 
-    const jsonData=JSON.stringify(data);
+    const jsonData = JSON.stringify(data);
 
-    const url="https://us1.api.mailchimp.com/3.0/lists/8ba058b952?";
+    const url = "https://us1.api.mailchimp.com/3.0/lists/8ba058b952?";
 
-    const options={
-        method:"POST",
-        auth:"Pulkit_asri:42c5bfa000c144ea47f5b85dbe7b246b-us1"
+    const options = {
+        method: "POST",
+        auth: "Pulkit_asri:42c5bfa000c144ea47f5b85dbe7b246b-us1"
     }
-    
-    const request=https.request(url,options,function(response){
-        response.on("data",function(data){
+
+    const request = https.request(url, options, function (response) {
+        response.on("data", function (data) {
             console.log(JSON.parse(data));
-
         });
-
-        console.log();
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
     });
 
     request.write(jsonData);
@@ -59,15 +59,17 @@ app.post("/", function (req, res) {
 
 });
 
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server Sunning On Port 3000");
 });
+
+
 //  Api Key 
 // 42c5bfa000c144ea47f5b85dbe7b246b-us1
-
-//Type of Data
-// {"name":"","contact":{"company":"","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"visibility":"pub","double_optin":false,"marketing_permissions":false}
 
 // List Id
 // 8ba058b952
